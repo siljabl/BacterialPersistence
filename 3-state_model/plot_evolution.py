c45a5156ee0a7ca31ad9bec52d9ac4a1d174be93
silja uio <s.b.lastad@mn.uio.no>
2024-12-08 14:42:00 +0100

    finished evolution figure

--- 3-state_model/plot_evolution.py
+++ 3-state_model/plot_evolution.py
@@ -1,38 +1,28 @@
 import sys
-import argparse
 import numpy as np
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 sys.path.append('src')
-from config_functions       import read_config
-from differential_equations import λ_min, δ_max
-from optimal_from_file      import identify_optimal_parameters_const_T0, identify_optimal_parameters_const_Tab
+from config_functions  import read_config
+from optimal_from_file import identify_optimal_parameters_const_T0, identify_optimal_parameters_const_Tab
 
-#mpl.rcParams["font.family"] = "serif"
-mpl.rcParams["font.size"]   = "12"
 
-parser = argparse.ArgumentParser(description='Competition between N species for tot_cycles cycles.')
-#parser.add_argument('folder', type=str, help="Folder for heatmap data.")
-#parser.add_argument('T0',     type=float, help='application time of antibiotics')
-#parser.add_argument('Tab',    type=float, help='duration of antibiotics')
-args = parser.parse_args()
 tot_cycles = 10_000
-
-p_arr = [0.1, 0.3, 0.5, 0.7, 0.9] #args.p
+p_arr = [0.1, 0.3, 0.5, 0.7, 0.9]
 index = [0, 1, 2, 3, 4]
 
 
-folder_T0_0 = 'data/constant_T0_0' #args.folder
+folder_T0_0 = 'data/constant_T0_0'
 folder_T0_2 = 'data/constant_Tab_12'
-Tab    = 12 #args.Tab
+Tab    = 12
 
 cmap = sns.color_palette('coolwarm', as_cmap=True)
 colors = cmap(np.linspace(0,1,5))
 
-sns.set_theme(style='ticks', font_scale=1.1)
-fig, ax = plt.subplots(2, 3, figsize=(6.75, 4.5), sharex=True)
+sns.set_theme(style='ticks', font_scale=1.2)
+fig, ax = plt.subplots(3, 2, figsize=(6.75, 7), sharex=True)
 
 
 for i in index:
@@ -49,12 +39,12 @@ for i in index:
     opt_params = identify_optimal_parameters_const_Tab(ab_params, config, folder_T0_2)
 
     ax[0,0].plot(λd / T, c=colors[i], alpha=0.7, label=f"{p:0.1}")
-    ax[0,1].plot(λr / T, c=colors[i], alpha=0.7, label=f"{p:0.1}")
-    ax[0,2].plot(δ,      c=colors[i], alpha=0.7, label=f"{p:0.1}")
+    ax[1,0].plot(λr / T, c=colors[i], alpha=0.7, label=f"{p:0.1}")
+    ax[2,0].plot(δ,      c=colors[i], alpha=0.7, label=f"{p:0.1}")
 
     ax[0,0].hlines(opt_params[0] / T, 0, tot_cycles, color=colors[i], ls="dashed")
-    ax[0,1].hlines(opt_params[1] / T, 0, tot_cycles, color=colors[i], ls="dashed")
-    ax[0,2].hlines(opt_params[2]    , 0, tot_cycles, color=colors[i], ls="dashed")
+    ax[1,0].hlines(opt_params[1] / T, 0, tot_cycles, color=colors[i], ls="dashed")
+    ax[2,0].hlines(opt_params[2]    , 0, tot_cycles, color=colors[i], ls="dashed")
 
 
 for i in index:
@@ -70,48 +60,37 @@ for i in index:
     ab_params  = {'p': p, 'T0': T0, 'Tab': Tab}
     opt_params = identify_optimal_parameters_const_T0(ab_params, config, folder_T0_0)
     
-    ax[1,0].plot(λd / T, c=colors[i], alpha=0.7, label=f"{p:0.1}")
+    ax[0,1].plot(λd / T, c=colors[i], alpha=0.7, label=f"{p:0.1}")
     ax[1,1].plot(λr / T, c=colors[i], alpha=0.7, label=f"{p:0.1}")
-    ax[1,2].plot(δ,      c=colors[i], alpha=0.7, label=f"{p:0.1}")
+    ax[2,1].plot(δ,      c=colors[i], alpha=0.7, label=f"{p:0.1}")
 
-    ax[1,0].hlines(opt_params[0] / T, 0, tot_cycles, color=colors[i], ls="dashed")
+    ax[0,1].hlines(opt_params[0] / T, 0, tot_cycles, color=colors[i], ls="dashed")
     ax[1,1].hlines(opt_params[1] / T, 0, tot_cycles, color=colors[i], ls="dashed")
-    ax[1,2].hlines(opt_params[2]    , 0, tot_cycles, color=colors[i], ls="dashed")
+    ax[2,1].hlines(opt_params[2]    , 0, tot_cycles, color=colors[i], ls="dashed")
 
 
-ax[0,0].set(title=r"$\langle \lambda \rangle ~/~ T$")
-ax[0,1].set(title=r"$\langle \omega  \rangle ~/~ T$")
-ax[0,2].set(title=r"$\langle \delta  \rangle$")
+ax[0,0].set(ylabel=r"$\langle \lambda \rangle ~/~ T$")
+ax[1,0].set(ylabel=r"$\langle \omega  \rangle ~/~ T$")
+ax[2,0].set(ylabel=r"$\langle \delta  \rangle$")
 
-for j in range(3):
-    ax[1,j].set(xlabel=r"Cycle $\times 10^3$")
+for j in range(2):
+    ax[2,j].set(xlabel=r"Cycle $\times 10^3$")
 
 for i in range(2):
-    ax[i,0].set(ylim=[0, 1.1],  yscale="linear", xscale="linear")
-    ax[i,1].set(ylim=[0, 1.1],  yscale="linear", xscale="linear")
-    ax[i,2].set(ylim=[0, 0.11], xlim=[0,1e4], yscale="linear", xscale="linear")
+    ax[0,i].set(ylim=[0, 1.1],  yscale="linear", xscale="linear")
+    ax[1,i].set(ylim=[0, 1.1],  yscale="linear", xscale="linear")
+    ax[2,i].set(ylim=[0, 0.11], xlim=[0,1e4], yscale="linear", xscale="linear")
 
 
-# Define a custom tick label formatting function
 def format_tick_label(x, pos):
     return f"{x/1000:.0f}"
 
 plt.gca().xaxis.set_major_formatter(mpl.ticker.FuncFormatter(format_tick_label))
 
-# fig.tight_layout(rect=[0, 0, 1, 0.85], h_pad=3)
-# ax[0,1].legend(loc='upper center',
-#                bbox_to_anchor=(.5, 1.9),
-#                ncol=5,
-#                frameon=False,
-#                fancybox=True,
-#                handlelength=1,
-#                shadow=False,
-#                title=r"$p$",
-#                alignment='left')
 
-fig.tight_layout(rect=[0, 0, 0.9, 1], h_pad=3)
-ax[0,1].legend(loc='upper center',
-               bbox_to_anchor=(3.2, 0.5),
+fig.tight_layout(rect=[0, 0, 0.87, 1], h_pad=3)
+ax[1,1].legend(loc='upper center',
+               bbox_to_anchor=(1.3, 1.15),
                ncol=1,
                reverse=True,
                frameon=False,
